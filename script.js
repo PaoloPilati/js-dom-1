0195764f58c9e908d659820d4057223437aaa7c3
Zizou10 <paolo.pilati93@gmail.com>
2025-12-19 16:27:12 +0100

    Semplificazione della funzione con rimozione della classe lamp-on ---> se l'obiettivo è disattivare una delle due immagini basta la classe con display: none. PROBLEMA: All'inizio appaiono entrambe le lampadine, poi la sequenza 0/1 si regolarizza.

--- script.js
+++ script.js
@@ -3,21 +3,15 @@
 //creare un collegamento allo switch (checkbox) che disattivi le classi lamp-off/lamp-on quando lo switch passa da 0 a 1 e viceversa
 
 const lampSwitch = document.getElementById('lampCheckChecked');
-const lampOn = document.querySelector('.lamp-on');
-const lampOff = document.querySelector('.lamp-off');
+const lampOff = document.getElementById('lamp-off');
+const lampOn = document.getElementById('lamp-on');
 
 
 //https://developer.mozilla.org/en-US/docs/Web/API/DOMTokenList/toggle
-function toggleLight (on, off) {
-
-    off.classList.toggle('lamp-off', lampSwitch.checked);
-    off.classList.toggle('lamp-on', !lampSwitch.checked);
-
-    on.classList.toggle('lamp-on', lampSwitch.checked);
+function toggleLight(on, off) {
     on.classList.toggle('lamp-off', !lampSwitch.checked);
-
+    off.classList.toggle('lamp-off', lampSwitch.checked);
 }
-toggleLight(lampOn, lampOff);
 
 lampSwitch.addEventListener('change', () => {
     toggleLight(lampOn, lampOff);
